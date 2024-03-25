71c9cb5d53a6567fc1a8cad5ed4f76e501d12dc1
Neil <nclark@argosafe.ed.ac.uk>
2024-03-25 11:41:56 +0000

    first commit

---
+++ 01_pull.R
@@ -0,0 +1,29 @@
+# Pull redcap team registraion
+
+library(tidyverse)
+
+#pull
+hospital_data_orig <- list("token"=Sys.getenv("redcap_token"),
+                           content='record',
+                           action='export',
+                           format='csv',
+                           type='flat',
+                           csvDelimiter='',
+                           rawOrLabel='raw',
+                           rawOrLabelHeaders='raw',
+                           exportCheckboxLabel='true',
+                           exportSurveyFields='false',
+                           exportDataAccessGroups='true',
+                           returnFormat='json',
+                           'forms[0]'='site_survey',
+                           'fields[0]'='data_collection_dag') %>% 
+  httr::POST(Sys.getenv("redcap_uri"), body = ., encode = "form") %>% 
+  httr::content()
+
+
+
+#apply factoring script
+data = hospital_data_orig
+source("GECKOTeamRegistratio-SiteSurveyDataNeil_R_2024-03-25_1128.r")
+hospital_data_orig = data
+rm(data)
